1e856060cac9aaa1a8ad33d1afa688d310242c75
Soo Yang, Tan <sooyang.code@gmail.com>
2017-07-13 18:41:47 +0800

    Add Text Input

---
+++ app/components/TextInput/InputWithButton.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, TouchableHighlight, TextInput } from 'react-native';
+import PropTypes from 'prop-types';
+import styles from './styles';
+import color from 'color';
+
+const InputWithButton = (props) => {
+    const { onPress, buttonText, editable = true } = props;
+    const underlayColor = color(styles.$buttonBackgroundColorBase).darken(styles.$buttonBackgroundColorModifier);
+    const containerStyles = [styles.container];
+    if (editable === false) {
+        containerStyles.push(styles.containerDisabled)
+    }
+
+    return(
+        <View style={containerStyles}>
+            <TouchableHighlight underlayColor={underlayColor} style={styles.buttonContainer} onPress={onPress}>
+                <Text style={styles.buttonText}> {buttonText} </Text>
+            </TouchableHighlight>
+            <View style={styles.border} />
+            <TextInput style={styles.input} underlineColorAndroid={'transparent'} {...props}/>
+        </View>
+    )
+};
+
+InputWithButton.PropTypes = {
+    onPress: PropTypes.func,
+    buttonText: PropTypes.string,
+    editable: PropTypes.bool,   
+}
+export default InputWithButton;
