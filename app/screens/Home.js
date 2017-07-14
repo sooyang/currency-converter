daa66572ecf57040e4d9ff07c78936ae55e1061a
Soo Yang, Tan <sooyang.code@gmail.com>
2017-07-14 11:12:49 +0800

    Add Header

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -6,6 +6,7 @@ import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Buttons';
 import {LastConverted} from '../components/Text';
+import { Header } from '../components/Header';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
@@ -31,10 +32,14 @@ class Home extends Component {
 		console.log('press swap currency');
 	}
 
+	handleOptionsPress = () => {
+		console.log('handle options press');
+	}
 	render() {
 		return (
 			<Container>
 				<StatusBar translucent={false} barStyle="light-content" />
+				<Header onPress={this.handleOptionsPress}/>
 				<Logo />
 				<InputWithButton 
 					buttonText={TEMP_BASE_CURRENCY}
