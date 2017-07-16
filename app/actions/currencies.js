65a72fbcf2608388c87916ced1b0bb131fd769a3
Soo Yang, Tan <sooyang.code@gmail.com>
2017-07-16 09:19:29 +0800

    Reducers

--- app/actions/currencies.js
+++ app/actions/currencies.js
@@ -1,7 +1,9 @@
 export const SWAP_CURRENCY = 'SWAP_CURRENCY';
+export const CHANGE_CURRENCY_AMOUNT = 'CHANGE_CURRENCY_AMOUNT';
 
-export const swapCurrency = () => ({
+export const swapCurrency = amount => ({
   type: SWAP_CURRENCY,
+  amount: parseFloat(amount),
 });
 
 export const changeCurrencyAmount = amount => ({
